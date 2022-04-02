6c8b350812cef1c634dfdc7e60949ed921c3a2fe
ejpham <ejpham1999@gmail.com>
2022-04-02 18:13:15 -0500

    sqlsrvconnect

---
+++ sqlsrvconnect.php
@@ -0,0 +1,18 @@
+<?php
+$serverName = "team-3-server.database.windows.net";
+$connectionOptions = array(
+    "Database" => "team-3-db",
+    "Uid" => "website",
+    "PWD" => "Password1!"
+);
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn) {
+    echo "Connection established.<br/>";
+}
+else {
+    echo "Connection could not be established.<br/>";
+    die(print_r(sqlsrv_errors(), true));
+}
+?>
