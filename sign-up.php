266d4f169d538c48929497a4be1171878337085f
ejpham <ejpham1999@gmail.com>
2022-04-02 18:40:01 -0500

    sign up system

--- sign-up.php
+++ sign-up.php
@@ -1,3 +1,45 @@
+<?php
+require_once "db_conn.php";
+require_once "session.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+    $name = trim($_POST['name']);
+    $email = trim($_POST['emai']);
+    $password = trim($_POST['password']);
+    $confirm_password = trim($_POST["confirm_password"]);
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+    
+    if ($query = $conn->prepare("SELECT * FROM WebLogins.users WHERE email = ?")) {
+        $error = '';
+        $query->bind_param('s', $email);
+        $query->execute();
+        $query->store_result();
+        if ($query->num_rows > 0) {
+            $error .= '<p class="error">Password must have at least 6 characters.</p>';
+        }
+        if (empty($confirm_password)) {
+            $error .= '<p class="error">Please confirm passwords.</p>';
+        }
+        else if (empty($error) && ($password != $confirm_password)) {
+            $error .= '<p class="error">Passwords did not match.</p>';
+        }
+        if (empty($error)) {
+            $insertQuery = $conn->prepare("INSERT INTO WebLogins.users (name, email, password) VALUES (?, ?, ?);");
+            $insertQuery->bind_param("sss", $name, $email, $password_hash);
+            $result = $insertQuery->execute();
+            if ($result) {
+                $error .= '<p class="success">Your registration was successful!</p>';
+            }
+            else {
+                $error .= '<p class="error">Something went wrong.</p>';
+            }
+        }
+        $query->close();
+        $insertQuery->close();
+        sqlsrv_close($conn);
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
