0dc40b937ceecf87b438fb509685aba483760308
Julian Garcia <garcia2002julian@gmail.com>
2022-04-03 22:58:18 -0500

    testing stuff out Julian

--- pricing.php
+++ pricing.php
@@ -46,6 +46,20 @@ session_start();
             </div>
         </nav>
     </div>
+    <div class="container-fluid col-sm-6">
+        <div class="row">
+            <div class="m-4">
+                <h6 class="display-6"> Pricing </h6>
+                <ul style = "list-style-type:square">
+
+                <li>Mail: $5</li>
+                <li>Packages size
+                    <li> 8 x 8 x 6: $10</li>
+                </li>
+                
+            </div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
