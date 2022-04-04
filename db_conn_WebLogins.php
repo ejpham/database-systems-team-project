3c05290d0807277ad56c3e8daefdd0fa19171806
ejpham <ejpham1999@gmail.com>
2022-04-04 00:50:06 -0500

    reverted

--- db_conn_WebLogins.php
+++ db_conn_WebLogins.php
@@ -1,6 +1,6 @@
 <?php
 define('serverName', 'database-cosc-3380-team-3.cp6t5hjmtlcw.us-east-1.rds.amazonaws.com');
-define('userName', 'website');
+define('userName', 'admin');
 define('password', 'Password1');
 define('dbName', 'WebLogins');
 $conn_WebLogins = mysqli_connect(serverName, userName, password, dbName);
