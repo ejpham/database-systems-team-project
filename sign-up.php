ce4001fa70fe186efe959d4a8d20e818a70bdab4
ejpham <ejpham1999@gmail.com>
2022-04-03 21:12:08 -0500

    commented code

--- sign-up.php
+++ sign-up.php
@@ -1,47 +1,70 @@
 <?php
 session_start();
+// check if already logged in, if so, redirect to index
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     header("location:index.php");
     exit;
 }
+// run script to connect to WebLogins schema in database
 require_once "db_conn_WebLogins.php";
+// declare variables
 $email = $name = $password = $confirm_password = "";
 $email_err = $name_err = $password_err = $confirm_password_err = $success = $error = "";
+// if "post" was called from submit button in form below
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // validate email from textbox
     if (empty(trim($_POST["email"]))) $email_err = "Please enter a valid e-mail address.";
     else if (strlen(trim($_POST["email"])) > 75) $email_err = "E-mail address can be no longer than 75 characters.";
     else $email = trim($_POST["email"]);
+    // validate name from textbox
     if (empty(trim($_POST["name"]))) $name_err = "Please enter a name.";
     else if (strlen(trim($_POST["name"])) > 75) $name_err = "Name can be no longer than 75 characters.";
     else $name = trim($_POST["name"]);
+    // validate password from textbox
     if (empty(trim($_POST["password"]))) $password_err = "Please enter a password.";
     else if (strlen(trim($_POST["password"])) < 6) $password_err = "Password must have at least 6 characters.";
     else if (strlen(trim($_POST["password"])) > 16) $password_err = "Password can be no longer than 16 characters.";
     else $password = trim($_POST["password"]);
+    // validate confirm password from textbox
     if (empty(trim($_POST["confirm_password"]))) $confirm_password_err = "Please confirm password.";
     else {
         $confirm_password = trim($_POST["confirm_password"]);
+        // make sure password === confirm password
         if (empty($password_err) && ($password != $confirm_password)) $confirm_password_err = "Password did not match.";
     }
+    
+    // variable containing query to search database for given email from textbox
     $check_db_email = "SELECT * FROM WebLogins.users WHERE email='$email'";
+    // run and store query result into variable
     $result = mysqli_query($conn_WebLogins, $check_db_email);
+    // boolean (i think...not sure but it works) variable from result
     $email_exists = mysqli_fetch_assoc($result);
+    // if true
     if ($email_exists) $email_err = "E-mail address is already taken.";
+    
+    // if all error strings are empty meaning all info is valid
     if (empty($email_err) && empty($name_err) && empty($password_err) && empty($confirm_password_err)) {
+        // variable containing query to insert info into database
         $sql = "INSERT INTO WebLogins.users (email, name, pass) VALUES (?, ?, ?)";
+        // prepare query statement
         if ($stmt = mysqli_prepare($conn_WebLogins, $sql)) {
+            // bind parameters into query statement
             mysqli_stmt_bind_param($stmt, "sss", $param_email, $param_name, $param_pass);
             $param_email = $email;
             $param_name = $name;
+            // encrypt password
             $param_pass = password_hash($password, PASSWORD_BCRYPT);
+            // if query executed successfully
             if (mysqli_stmt_execute($stmt)) {
                 $success = '<div class="alert alert-success" role="alert">Your account has been created.</div>';
+                // wait 2 seconds then redirect to sign-in page
                 header('refresh:2; url=sign-in.php');
             }
             else $error = '<div class="alert alert-danger" role="alert">Oops, something went wrong. Please try again later.</div>';
         }
         else $error = '<div class="alert alert-danger" role="alert">Please make sure your information is valid.</div>';
     }
+    // close the connection
     mysqli_close($conn_WebLogins);
 }
 ?>
