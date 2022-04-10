cb94a2302336a9572f82533e194562b913901ac8
Julian Garcia <garcia2002julian@gmail.com>
2022-04-09 19:06:43 -0500

    Julian

--- mail.php
+++ mail.php
@@ -1,5 +1,30 @@
 <?php
 session_start();
+require_once "db_conn_WebLogins.php";
+require_once "db_conn_PostalService.php";
+$name = $email = $message = $mail_type = "";
+$name_err = $email_err  = $success = $error = $mail_error"";
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    $email = $_SESSION["email"];
+    $grab_name_sql = "SELECT name FROM WebLogins.users WHERE email='$email'";
+    $result = mysqli_query($conn_WebLogins, $grab_name_sql);
+    $name = mysqli_fetch_assoc($result)["name"];
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+        if (empty(trim($_POST["name"]))) $name_err = "Please enter a name.";
+        else if (strlen(trim($_POST["name"])) > 75) $name_err = "Name can be no longer than 75 characters.";
+        else $name = trim($_POST["name"]);
+        if (empty(trim($_POST["email"]))) $email_err = "Please enter a valid e-mail address.";
+        else if (strlen(trim($_POST["email"])) > 75) $email_err = "E-mail address can be no longer than 75 characters.";
+        else $email = trim($_POST["email"]);
+
+        if (trim($_POST["mailtype"]) == "Select Mail Type") $mail_error = "Please select mail type.";
+        else $mail_type = trim($_POST["mailtype"]);
+    }
+
+    
+}
 ?>
 
 <!DOCTYPE html>
@@ -49,6 +74,67 @@ session_start();
             </div>
         </nav>
     </div>
+
+    <div class="container-fluid col-sm-6">
+        <div class="row">
+            <div class="m-4">
+                <h6 class="display-6">Sending Mail</h6>
+                <p>Fill out the form below to send mail.</p>
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                    <?php
+                        echo $success;
+                        echo $error;
+                        if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
+                        <div class="m-3">
+                            <label class="form-label">Full Name</label>
+                            <input type="text" name="name" class="form-control-plaintext <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>" id="inputName" value="<?php echo $name ?>" disabled>
+                            <span class="invalid-feedback"><?php echo $name_err; ?></span>
+                        </div>
+                        <div class="m-3">
+                            <label class="form-label" for="inputEmail">E-mail Address</label>
+                            <input type="email" name="email" class="form-control-plaintext <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>" id="inputEmail" value="<?php echo $email ?>" disabled>
+                            <span class="invalid-feedback"><?php echo $email_err; ?></span>
+                        </div>
+                    <?php } else { ?>
+                        <div class="m-3">
+                            <label class="form-label">Full Name</label>
+                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>" id="inputName" placeholder="Full Name">
+                            <span class="invalid-feedback"><?php echo $name_err; ?></span>
+                        </div>
+                        <div class="m-3">
+                            <label class="form-label" for="inputEmail">E-mail Address</label>
+                            <input type="email" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>" id="inputEmail" placeholder="E-mail Address">
+                            <span class="invalid-feedback"><?php echo $email_err; ?></span>
+                        </div>
+                    <?php } ?>
+                    <div class="m-3">
+                        <select class="form-select" aria-label="Default select example" type = "mailtype">
+                            <option selected>Select Mail Type</option>
+                            <option value="Letter">Letter</option>
+                            <option value="Package">Package</option>
+                        </select>
+                    </div>
+
+                    <div class="m-3">
+                        <input type="submit" name="submit" class="btn btn-outline-secondary" value="Send">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- <script type="text/javascript">
+        $('select').change(function() {
+            $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue){
+                $(".box").not("." + optionValue).hide();
+                $("." + optionValue).show();
+            } else{
+                $(".box").hide();
+            }
+        });
+    </script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
