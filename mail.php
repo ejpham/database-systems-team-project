de856be6b0f0114c7065093381b1c0861d5953ca
Julian Garcia <garcia2002julian@gmail.com>
2022-04-09 20:35:34 -0500

    Julian

--- mail.php
+++ mail.php
@@ -2,8 +2,8 @@
 session_start();
 require_once "db_conn_WebLogins.php";
 require_once "db_conn_PostalService.php";
-$name = $email = $message = $mail_type = "";
-$name_err = $email_err = $mail_type_err = $success = $error = "";
+$name = $email = $message = $mail_type = $address = $city = "";
+$name_err = $email_err = $mail_type_err = $success = $error = $address_err $city_err = "";
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     $email = $_SESSION["email"];
     $grab_name_sql = "SELECT name FROM WebLogins.users WHERE email='$email'";
@@ -105,6 +105,72 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <span class="invalid-feedback"><?php echo $email_err; ?></span>
                         </div>
                     <?php } ?>
+
+                    <div class="m-3">
+                        <label class="form-label">From Address</label>
+                        <input type="text" name="Address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $address; ?>" id="inputAddress" placeholder="To Address">
+                        <span class="invalid-feedback"><?php echo $address_err; ?></span>
+                    </div>
+
+                    <div class="m-3">
+                        <select class="form-select" aria-label="Default select example" type = "state" id = "stateSelector">
+                        <option value="AL">AL</option>
+                            <option value="AK">AK</option>
+                            <option value="AR">AR</option>	
+                            <option value="AZ">AZ</option>
+                            <option value="CA">CA</option>
+                            <option value="CO">CO</option>
+                            <option value="CT">CT</option>
+                            <option value="DC">DC</option>
+                            <option value="DE">DE</option>
+                            <option value="FL">FL</option>
+                            <option value="GA">GA</option>
+                            <option value="HI">HI</option>
+                            <option value="IA">IA</option>	
+                            <option value="ID">ID</option>
+                            <option value="IL">IL</option>
+                            <option value="IN">IN</option>
+                            <option value="KS">KS</option>
+                            <option value="KY">KY</option>
+                            <option value="LA">LA</option>
+                            <option value="MA">MA</option>
+                            <option value="MD">MD</option>
+                            <option value="ME">ME</option>
+                            <option value="MI">MI</option>
+                            <option value="MN">MN</option>
+                            <option value="MO">MO</option>	
+                            <option value="MS">MS</option>
+                            <option value="MT">MT</option>
+                            <option value="NC">NC</option>	
+                            <option value="NE">NE</option>
+                            <option value="NH">NH</option>
+                            <option value="NJ">NJ</option>
+                            <option value="NM">NM</option>			
+                            <option value="NV">NV</option>
+                            <option value="NY">NY</option>
+                            <option value="ND">ND</option>
+                            <option value="OH">OH</option>
+                            <option value="OK">OK</option>
+                            <option value="OR">OR</option>
+                            <option value="PA">PA</option>
+                            <option value="RI">RI</option>
+                            <option value="SC">SC</option>
+                            <option value="SD">SD</option>
+                            <option value="TN">TN</option>
+                            <option value="TX">TX</option>
+                            <option value="UT">UT</option>
+                            <option value="VT">VT</option>
+                            <option value="VA">VA</option>
+                            <option value="WA">WA</option>
+                            <option value="WI">WI</option>	
+                            <option value="WV">WV</option>
+                            <option value="WY">WY</option>
+                        </select>
+                        <span class="input-group-text">@</span>
+                        <input type="text" name="City" class="form-control <?php echo (!empty($city_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $city; ?>" id="inputCity" placeholder="city">
+                        <span class="invalid-feedback"><?php echo $address_err; ?></span>
+                    </div>
+
                     <div class="m-3">
                         <select class="form-select" aria-label="Default select example" type = "mailtype" id = "mailSelector" onchange = "MailCheck(this);">
                             <option value = "">Select Mail Type</option>
@@ -144,7 +210,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
 
                     <div class="m-3" id = "packageWeight" style="display: none;">
-                        <label for="weight" class="form-label">Slide for weight</label>
+                        <label for="weight" class="form-label">Slide for weight (round down)</label>
                         <input type="range" class="form-range" min="0" max="100" step = "10" id="weight">
 
                     </div>
