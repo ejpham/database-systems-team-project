d5b3dab4c91ac6e68f0abc5a990cb74bfb9a4c01
Julian Garcia <garcia2002julian@gmail.com>
2022-04-12 02:44:04 -0500

    Julian

--- mail.php
+++ mail.php
@@ -343,6 +343,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
 
+        window.onload = MailCheck(document.getElementById("mailSelector"));
+    </script>
+
+    <script type = "text/javascript">
         function updatePrice(){
             var adding = 0;
             const array = document.getElementById("packageSize").value.split(":");
@@ -350,8 +354,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             priceChanging = $('#priceChanging');
             priceChanging.text(adding));
         }
-
-        window.onload = MailCheck(document.getElementById("mailSelector"));
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
