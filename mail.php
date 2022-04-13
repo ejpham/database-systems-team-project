0dc2e0e8f2d963182f9af432ccd21b689ab50400
Julian Garcia <garcia2002julian@gmail.com>
2022-04-13 00:54:37 -0500

    Julian

--- mail.php
+++ mail.php
@@ -77,7 +77,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (empty(trim($_POST["expDate"]))) $expDate_err = "Please enter a valid Expiration Date.";
         else if (strlen(trim($_POST["expDate"])) != 5) $expDate_err = "Please enter a valid Expiration Date.";
         else{
-            if(intval(substr($_POST["expDate"])), 0, 2) > 12)
+            if(intval(substr($_POST["expDate"])), 0, 1) > 12)
                 $expDate_err = "Please enter a valid month";/*
             else if(intval(substr($_POST["expDate"]), 3, 2) < 22)
                 $expDate_err = "Please enter a valid year";*/
