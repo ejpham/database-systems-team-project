4abf53aceede84a85809bbb2f00211be015d7e33
Julian Garcia <garcia2002julian@gmail.com>
2022-04-14 23:20:33 -0500

    Fixing

--- mail.php
+++ mail.php
@@ -143,28 +143,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 function checkErrors(){
-    echo $email_err;
-    echo $name_err;
-    echo $message_err;
-    echo $price;
-    echo $fromaddress_err;
-    echo $fromcity_err;
-    echo $fromstate_err;
-    echo $fromZip_err;
-    echo $toZip_err;
-    echo $address_err;
-    echo $city_err;
-    echo $state_err;
-    echo $mail_type_err;
-    echo $recName_err;
-    echo $packSpeed_err;
-    echo $lettSpeed_err;
+    // echo $email_err;
+    // echo $name_err;
+    // echo $message_err;
+    
+    // echo $fromaddress_err;
+    // echo $fromcity_err;
+    // echo $fromstate_err;
+    // echo $fromZip_err;
+    // echo $toZip_err;
+    // echo $address_err;
+    // echo $city_err;
+    // echo $state_err;
+    // echo $mail_type_err;
+    // echo $recName_err;
+    // echo $packSpeed_err;
+    // echo $lettSpeed_err;
     
     if(empty($email_err) && empty($name_err) && empty($message_err) && $price != 0 && empty($fromaddress_err) && empty($fromcity_err) && empty($fromstate_err) && empty($fromZip_err) && empty($toZip_err) && empty($address_err) && empty($city_err) && empty($state_err) && empty($mail_type_err) && empty($recName_err) && (empty($packSpeed_err) || empty($lettSpeed_err))){
         echo "work";
         return true;
     }
     else{
+        echo $price;
         echo "no work";
         return false;
     }
